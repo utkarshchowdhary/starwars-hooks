04577b9dadb281575c161a0e927da9b017f3adf7
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-11-13 23:57:26 +0530

    implemented pwa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import 'normalize.css/normalize.css';
 import './styles/styles.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+serviceWorker.register();
