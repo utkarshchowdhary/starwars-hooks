b49bf202fb0f1d60ed622ff33e26debff74a5cbc
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2020-11-13 23:46:31 +0530

    Updates

---
+++ precache-manifest.ffc90e05c42ba30c42946387c2bd9cd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ff1db8d345429e90b8415a42fd0b9f",
+    "url": "/starwars-hooks/index.html"
+  },
+  {
+    "revision": "3bf7a31347631a6673d1",
+    "url": "/starwars-hooks/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0d14c9f005df4b666def",
+    "url": "/starwars-hooks/static/css/main.88492e84.chunk.css"
+  },
+  {
+    "revision": "3bf7a31347631a6673d1",
+    "url": "/starwars-hooks/static/js/2.517a42a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starwars-hooks/static/js/2.517a42a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d14c9f005df4b666def",
+    "url": "/starwars-hooks/static/js/main.fa74947b.chunk.js"
+  },
+  {
+    "revision": "93a3cd341595a5006cd7",
+    "url": "/starwars-hooks/static/js/runtime-main.f9a441ed.js"
+  }
+]);
